b6f1ca50b9a5c4fcf8e439b7cfc05a97de98de7d
Héctor <hectoruu@gmail.com>
2025-01-27 16:52:57 -0600

    refactor(HU-001): Ajustes en el constructor y getters

--- src/main/java/com/banco/sistema/Usuario.java
+++ src/main/java/com/banco/sistema/Usuario.java
@@ -14,4 +14,36 @@ public class Usuario {
         this.contrasena = contrasena;
     }
 
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 }
